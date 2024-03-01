16fef697bf7f7b18c016ebdfa385080bdc24ecd7
Ismoil98 <ismoil6366>
2024-03-01 16:15:17 +0500

    added

--- src/components/command/command.jsx
+++ src/components/command/command.jsx
@@ -8,6 +8,83 @@ class command extends Component {
         <h2>
             Наша <span>команда</span>
         </h2>
+        <ul>
+            <li>
+                <img src="command1.png" alt="" />
+                <h3>
+                    Войнич Дарья
+                </h3>
+                <p className='info'>
+                    Заместитель директора по продажам
+                </p>
+                <p className='tel'>
+                    +375 (17) 270-53-77 (317)
+                </p>
+                <p className='mail'>
+                    k.melnichenko@leangroup.by
+                </p>
+            </li>
+            <li>
+                <img src="command2.png" alt="" />
+                <h3>
+                    Войнич Дарья
+                </h3>
+                <p className='info'>
+                    Заместитель директора по продажам
+                </p>
+                <p className='tel'>
+                    +375 (17) 270-53-77 (317)
+                </p>
+                <p className='mail'>
+                    k.melnichenko@leangroup.by
+                </p>
+            </li>
+            <li>
+                <img src="command3.png" alt="" />
+                <h3>
+                    Войнич Дарья
+                </h3>
+                <p className='info'>
+                    Заместитель директора по продажам
+                </p>
+                <p className='tel'>
+                    +375 (17) 270-53-77 (317)
+                </p>
+                <p className='mail'>
+                    k.melnichenko@leangroup.by
+                </p>
+            </li>
+            <li>
+                <img src="command4.png" alt="" />
+                <h3>
+                    Войнич Дарья
+                </h3>
+                <p className='info'>
+                    Заместитель директора по продажам
+                </p>
+                <p className='tel'>
+                    +375 (17) 270-53-77 (317)
+                </p>
+                <p className='mail'>
+                    k.melnichenko@leangroup.by
+                </p>
+            </li>
+            <li>
+                <img src="command5.png" alt="" />
+                <h3>
+                    Войнич Дарья
+                </h3>
+                <p className='info'>
+                    Заместитель директора по продажам
+                </p>
+                <p className='tel'>
+                    +375 (17) 270-53-77 (317)
+                </p>
+                <p className='mail'>
+                    k.melnichenko@leangroup.by
+                </p>
+            </li>
+        </ul>
       </div>
     )
   }
